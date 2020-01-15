83c6c730b9336586f97e29976b690eae02ff3992
Simon Rogers <simon.d.rogers@gmail.com>
2020-01-15 11:27:45 +0000

    added DSK process class

--- code/filtering.py
+++ code/filtering.py
@@ -3,8 +3,9 @@
 # Filtering the matrix 
 
 class Filter(object):
-    def process(self):
+    def process(self,input_matrix,kmer_list,strain_list):
         raise NotImplemented()
 
 class SimpleFilter(Filter):
-    
+    def process(self,input_matrix,kmer_list,strain_list):
+        
