151ec902f01a03bf9cafe18bdd816ae280330f47
Simon Rogers <simon.d.rogers@gmail.com>
2020-01-15 15:50:06 +0000

    tidied and fixed bug in filtering.py

--- code/filtering.py
+++ code/filtering.py
@@ -15,31 +15,38 @@ class SimpleFilter(Filter):
         self.max_presence = max_presence
 
     def process(self,data_list,kmer_idx,strain_idx):
-        retain = set()
+        
+
+        # create a reverse idx as positions are stored in data_list
         reverse_kmer_idx = {v:k for k,v in kmer_idx.items()}
 
+        # compute in how many strains each kmer appears
         kmer_counts = {k:0 for k in kmer_idx}
         for kmer_pos,strain_pos,count in data_list:
             kmer = reverse_kmer_idx[kmer_pos]
             kmer_counts[kmer] += 1        
 
-
+        # keep only those kmers that fit within the range
+        retain = set()
         for kmer,count in kmer_counts.items():
             if count >= self.min_presence and count <= self.max_presence:
                 retain.add(kmer)
         
-        # make a new kmer index
+        # make a new kmer index as we will be removing some
         new_kmer_idx = {}
         new_kmer_pos = 0
         new_data_list = []
         for row,col,count in data_list:
-                kmer = reverse_kmer_idx[row]
-                if kmer in retain:
-                    if not kmer in new_kmer_idx:
-                        new_kmer_idx[kmer] = new_kmer_pos
-                        new_kmer_pos += 1
-
-                        new_row = new_kmer_idx[kmer]
-                        new_data_list.append((new_row,col,count))
-        
+            # get the kmer from the position
+            kmer = reverse_kmer_idx[row]
+            # if it is one we're keeping...
+            if kmer in retain:
+                # if we haven't seen it before add it to the new index
+                if not kmer in new_kmer_idx:
+                    new_kmer_idx[kmer] = new_kmer_pos
+                    new_kmer_pos += 1
+                # add the data item to the new list
+                new_row = new_kmer_idx[kmer]
+                new_data_list.append((new_row,col,count))
+    
         return new_data_list,new_kmer_idx,strain_idx
