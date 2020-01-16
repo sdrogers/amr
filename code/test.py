5c7c6354417f6da94a7ce273415f91afada45c58
Simon Rogers <simon.d.rogers@gmail.com>
2020-01-16 12:05:20 +0000

    modified test.py to test filtering and processing on uist

--- code/test.py
+++ code/test.py
@@ -1,13 +1,22 @@
 #test.py
 
 from process_dsk import DSKProcess
+import glob
+import os
 
-d = DSKProcess(['/Users/simon/git/amr/example_data/dsk/dskTxtExample.txt'])
+dsk_folder = '/srv/data/amr/sample_data/strain_kmers'
+dsk_file_list = glob.glob(os.path.join(dsk_folder,'*.txt'))
+
+
+d = DSKProcess(dsk_file_list)
 data_list,kmer_idx,strain_idx = d.process()
-print(len(data_list))
+print("n_kmers {}, n_strains {}".format(len(kmer_idx),len(strain_idx)))
 
 from filtering import SimpleFilter
-s = SimpleFilter(min_presence = 1)
+s = SimpleFilter(min_presence = round(0.05*len(kmer_idx)),max_presence = round(0.95*len(strain_idx)))
 new_data_list,new_kmer_idx,stain_idx = s.process(data_list,kmer_idx,strain_idx)
 
-print(len(new_data_list))
+from scipy.stats import coo_matrix
+
+i,j,k = zip(*new_data_list)
+coo = coo_matrix((k,(i,j)),shape = (len(new_kmer_idx),len(strain_idx)))
