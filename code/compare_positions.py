704a83218e85cfb83df7d61cbd31d2754fdfcff1
Grimur Hjorleifsson <grimurh@gmail.com>
2020-01-23 11:34:13 +0000

    Cleanup, bugfixes, command line arguments

--- code/compare_positions.py
+++ code/compare_positions.py
@@ -63,32 +63,7 @@ def load_reference_data(filename):
     return reference
 
 
-def check_overlap(start, stop, pos_list, margin=10):
-    start_positions = [x[0] for x in pos_list]
-
-    start_pos = 0
-    end_pos = len(start_positions) - 1
-
-    print(start_pos, end_pos)
-
-    while start_pos != end_pos:
-        d = max(1, (end_pos - start_pos) / 2)
-        i = start_pos + d
-        if start_positions[i] < start:
-            if i != start_pos:
-                start_pos = i
-            else:
-                end_pos = i
-        else:
-            if end_pos != i:
-                end_pos = i
-            else:
-                start_pos = i
-
-    print(i)
-
-
-def check_overlap_seq(start, stop, pos_list, margin=10):
+def check_overlap_seq(start, stop, pos_list, margin):
     for start_pos, stop_pos, obj_id in pos_list:
         if stop > start_pos - margin and start < stop_pos + margin:
             yield start_pos, stop_pos, obj_id
@@ -98,7 +73,7 @@ def check_overlap_seq(start, stop, pos_list, margin=10):
             break
 
 
-def compare_files(file_1, file_2):
+def compare_files(file_1, file_2, margin):
     # Use the smaller file as reference and loop through the larger one
     if file_len(file_1) < file_len(file_2):
         base_file = file_1
@@ -112,8 +87,10 @@ def compare_files(file_1, file_2):
     for strain_id, contig_id, object_id, start, stop in parse_file(check_file):
         if strain_id in reference_data:
             if contig_id in reference_data[strain_id]:
-                for start_pos, stop_pos, obj_id in check_overlap_seq(start, stop, reference_data[strain_id][contig_id]):
-                    print(obj_id, start_pos, stop_pos, object_id, start, stop)
+                for start_pos, stop_pos, obj_id in check_overlap_seq(start, stop, reference_data[strain_id][contig_id], margin):
+                    output_fields = (strain_id, contig_id, obj_id, start_pos, stop_pos, object_id, start, stop)
+                    output = '\t'.join([str(x) for x in output_fields])
+                    print(output)
 
 
 def test_check_overlap_seq():
@@ -130,9 +107,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('f1')
     parser.add_argument('f2')
+    parser.add_argument('-d', dest='margin', help='distance btw. BGC and k-mer', type=int, required=False, default=10)
     args = parser.parse_args()
 
     file1 = args.f1
     file2 = args.f2
+    margin = args.margin
 
-    compare_files(file1, file2)
+    compare_files(file1, file2, margin)
